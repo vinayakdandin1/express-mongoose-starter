f936f36fd961d46febfd51063f897cb8802b00ae
Manish Poduval <manishpoduval@hotmail.com>
2021-02-05 10:12:21 +0530

    updated package versions

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 var hbs = require('hbs');
-const logger = require('morgan');
+const morgan = require('morgan');
 const PORT = process.env.PORT
 
 //ensure database is connected
-require('./config/db.config')
+// We connect to our local database here called `todos`
+mongoose.connect('mongodb://localhost/todos', { useNewUrlParser: true,
+useUnifiedTopology: true,})
+    .then((self) => {
+        console.log('Yayyy Database is connected');
+    })
+    .catch(() => {
+        console.log('Something went wrong with db connection!');
+    })
+
 
 // Register your template engine
 // NOTE: 'view engine' is a keyword here. 
@@ -26,7 +36,7 @@ app.use(express.static(__dirname + '/public'))
 
 
 //Allows us to see detailed logs in the console
-app.use(logger('dev'));
+app.use(morgan('dev'));
 
 //Register partials if needed
 //hbs.registerPartials(__dirname + '/views/partials');
